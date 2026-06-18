b1fdf99695c0f16a8f9ceb4d74f882a17f59d061
ali-hue-byte <elguelouiali@gmail.com>
2026-06-18 20:42:47 +0200

    Update dynamic_array.h

--- Array/dynamic_array.h
+++ Array/dynamic_array.h
@@ -16,9 +16,10 @@ void dynamic_array_insert(Dynamic_Array *d_array, int index, int value);
 void dynamic_array_update(Dynamic_Array *d_array, int index, int value);
 void dynamic_array_delete(Dynamic_Array *d_array, int index);
 int get_dynamic_array_length(Dynamic_Array *d_array);
-Dynamic_Array creat_dynamic_array();
+Dynamic_Array create_dynamic_array();
 void empty_dynamic_array(Dynamic_Array *d_array);
 void print_dynamic_array(Dynamic_Array *d_array);
 bool is_empty_dynamic_array(Dynamic_Array *d_array);
+void destroy_dynamic_array(Dynamic_Array *d_array);
 
 #endif
