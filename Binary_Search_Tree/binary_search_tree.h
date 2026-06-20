3e4d40bbd5a6b571a2b240d723466283b05fd4e9
ali-hue-byte <elguelouiali@gmail.com>
2026-06-20 19:47:32 +0200

    Create binary_search_tree.h

---
+++ Binary_Search_Tree/binary_search_tree.h
@@ -0,0 +1,25 @@
+#ifndef BINARYSEARCHTREE_H
+#define BINARYSEARCHTREE_H
+
+typedef struct Node{
+    int value;
+    struct Node* left;
+    struct Node* right;
+    struct Node* parent;
+    
+} Node;
+
+typedef struct BST{
+    Node* root;
+    int size;
+} BST;
+
+BST create_BST();
+void BST_insert(BST *bst, int value);
+void BST_delete(BST *bst, int value);
+int BST_length(BST *bst);
+bool is_empty_BST(BST *bst);
+bool is_leaf_BST_Node(Node *node);
+void print_BST(BST *bst);
+
+#endif
