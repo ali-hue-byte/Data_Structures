b067c950ca94ec352b209a3c39382412452e248e
ali-hue-byte <elguelouiali@gmail.com>
2026-06-21 10:34:05 +0200

    Update linked_list.h

--- Linked_List/linked_list.h
+++ Linked_List/linked_list.h
@@ -3,30 +3,30 @@
 
 typedef struct Circular_Double_Linked_List Circular_Double_Linked_List;
 
-typedef struct Node{
+typedef struct CDLL_Node{
     int data;
-    struct Node *next;
-    struct Node *previous;
+    struct CDLL_Node *next;
+    struct CDLL_Node *previous;
     Circular_Double_Linked_List *CDLL;
-} Node;
+} CDLL_Node;
 
 struct Circular_Double_Linked_List{
-    Node *head;
+    CDLL_Node *head;
     int size;
 };
 
 Circular_Double_Linked_List creat_CDLL();
-Node *CDLL_push_back(Circular_Double_Linked_List *lst, int value);
-Node *CDLL_push_front(Circular_Double_Linked_List *lst, int value);
-Node *CDLL_insert_after(Circular_Double_Linked_List *lst, Node *node, int value);
-Node *CDLL_insert_before(Circular_Double_Linked_List *lst, Node *node, int value);
-int CDLL_get_node_value(Circular_Double_Linked_List *lst, Node *node);
+CDLL_Node *CDLL_push_back(Circular_Double_Linked_List *lst, int value);
+CDLL_Node *CDLL_push_front(Circular_Double_Linked_List *lst, int value);
+CDLL_Node *CDLL_insert_after(Circular_Double_Linked_List *lst, CDLL_Node *node, int value);
+CDLL_Node *CDLL_insert_before(Circular_Double_Linked_List *lst, CDLL_Node *node, int value);
+int CDLL_get_node_value(Circular_Double_Linked_List *lst, CDLL_Node *node);
 int CDLL_front(Circular_Double_Linked_List *lst);
 int CDLL_back(Circular_Double_Linked_List *lst);
 int CDLL_pop_front(Circular_Double_Linked_List *lst);
 int CDLL_pop_back(Circular_Double_Linked_List *lst);
-void CDLL_update_node_value(Circular_Double_Linked_List *lst, Node *node, int value);
-void CDLL_delete_node(Circular_Double_Linked_List *lst, Node *node);
+void CDLL_update_node_value(Circular_Double_Linked_List *lst, CDLL_Node *node, int value);
+void CDLL_delete_node(Circular_Double_Linked_List *lst, CDLL_Node *node);
 void print_CDLL(Circular_Double_Linked_List *lst);
 int CDLL_get_length(Circular_Double_Linked_List *lst);
 bool CDLL_is_empty(Circular_Double_Linked_List *lst);
