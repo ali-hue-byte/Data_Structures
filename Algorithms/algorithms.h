8e705934a55bf97e517efaef441503a439b17ade
ali-hue-byte <elguelouiali@gmail.com>
2026-06-21 12:04:47 +0200

    Create algorithms.h

---
+++ Algorithms/algorithms.h
@@ -0,0 +1,8 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+
+Dynamic_Array remove_duplicates_dynamic_array(Dynamic_Array *d_arr);
+Dynamic_Array sort_dynamic_array(Dynamic_Array *d_arr);
+Hash_Table words_frequency(char *text);
+#endif
