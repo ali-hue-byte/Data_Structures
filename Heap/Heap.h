e3e4f7e212030633bc31f705705a6fa2cd81feb2
ali-hue-byte <elguelouiali@gmail.com>
2026-06-22 15:39:44 +0200

    Update Heap.h

--- Heap/Heap.h
+++ Heap/Heap.h
@@ -14,5 +14,8 @@ void heap_add(Heap *heap, int value);
 int heap_peek(Heap *heap);
 int heap_extract(Heap *heap);
 bool is_empty_heap(Heap *heap);
+int heap_length(Heap *heap);
+void print_heap(Heap *heap);
+void empty_heap(Heap *heap);
 
 #endif
