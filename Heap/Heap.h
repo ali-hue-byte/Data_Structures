6233973713f9453f163accd1d61d9aeb07bd42a7
ali-hue-byte <elguelouiali@gmail.com>
2026-06-22 15:31:46 +0200

    Create Heap.h

---
+++ Heap/Heap.h
@@ -0,0 +1,18 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+
+
+#include "../Array/dynamic_array.h"
+typedef struct {
+    Dynamic_Array heap_arr;
+    bool is_max;
+} Heap;
+
+Heap create_heap(bool is_max);
+void heap_add(Heap *heap, int value);
+int heap_peek(Heap *heap);
+int heap_extract(Heap *heap);
+bool is_empty_heap(Heap *heap);
+
+#endif
