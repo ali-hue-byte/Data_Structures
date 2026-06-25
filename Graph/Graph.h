b0b9e62ccfd061a40f9fdafe79ed402cd7127fe5
ali-hue-byte <elguelouiali@gmail.com>
2026-06-25 16:19:57 +0200

    Update Graph.h

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -1,38 +1,7 @@
 #ifndef Graph_H
 #define Graph_H
-
 #include "../Array/dynamic_array.h"
-
-typedef struct Vertex Vertex;
-
-typedef struct elem_Graph{
-   int key;
-   Vertex *value;
-   struct elem_Graph *next;
-}elem_Graph;
-
-typedef struct {
-    elem_Graph **pair;
-    int size;
-    int capacity;
-}Hash_Table_Graph;
-
-
-int Hash_function(int key, int capacity);
-
-Hash_Table_Graph  create_hash_table_graph();
-bool is_empty_hash_table_graph(Hash_Table_Graph *h_t);
-bool key_in_hash_table_graph(Hash_Table_Graph *h_t, int key);
-void hash_table_graph_add(Hash_Table_Graph *h_t, int key, Vertex *value);
-Vertex *hash_table_graph_get(Hash_Table_Graph *h_t, int key);
-void hash_table_graph_delete(Hash_Table_Graph *h_t, int key);
-void hash_table_graph_empty(Hash_Table_Graph *h_t);
-void destroy_hash_table_graph(Hash_Table_Graph *h_t);
-int hash_table_graph_size(Hash_Table_Graph *h_t);
-void hash_table_graph_update(Hash_Table_Graph *h_t, int key, Vertex *value);
-
-
-
+#include "Hash_Table_Graph.h"
 
 
 typedef struct Edge { 
