e9bb330551a6f67807f0c4350338edbcff8ead0f
ali-hue-byte <elguelouiali@gmail.com>
2026-06-25 14:28:35 +0200

    Update Graph.c

--- Graph/Graph.c
+++ Graph/Graph.c
@@ -5,7 +5,6 @@
 
 
 
-
 Graph create_graph(bool is_weighted, bool is_direct){
     Graph graph;
     
@@ -167,6 +166,50 @@ int get_edge_weight_graph(Graph *graph, int from, int to, bool is_id){
     
 }
 
+Dynamic_Array get_neighbors_ids(Graph *graph, int id){
+    if(!vertex_id_exists_graph(graph, id)){
+        printf("Id doesn't exist\n");
+        return create_dynamic_array();
+    }
+    Dynamic_Array d_array = create_dynamic_array();
+    Vertex *v = hash_table_graph_get(&graph->vertices, id);
+    Edge *current = v->Edges_list;
+    while(current != NULL){
+        dynamic_array_append(&d_array, current->destination_id);
+        current = current->next_Edge;
+    }
+    return d_array;
+
+}
+
+Dynamic_Array get_neighbors_values(Graph *graph, int value){
+
+    elem_Graph *current = NULL;
+    for(int i = 0; i < graph->vertices.capacity; i++){
+        if(current != NULL) break;
+        current = graph->vertices.pair[i];
+        while(current != NULL){
+            if(current->value->value == value) break;
+            current = current->next;
+        }
+    }
+    if(current == NULL || current->value->value != value){
+        printf("Value doesn't exist\n");
+        return create_dynamic_array();
+    }
+    
+    Dynamic_Array d_array = create_dynamic_array();
+
+    Edge *edge = current->value->Edges_list;
+    while(edge != NULL){
+        Vertex *v = hash_table_graph_get(&graph->vertices, edge->destination_id);
+        dynamic_array_append(&d_array, v->value);
+        edge = edge->next_Edge;
+    }
+    return d_array;
+}
+
+
 bool edge_exists_graph(Graph *graph, int from, int to, bool is_id){
     if (is_id){
         Vertex *v_from = hash_table_graph_get(&graph->vertices, from);
@@ -235,3 +278,334 @@ bool vertex_value_exists_graph(Graph *graph, int value){
     return false;
 }
 
+void print_graph_ids(Graph *graph){
+    if(is_empty_graph(graph)) {
+        printf("Graph is empty\n");
+        return;
+    }
+
+    printf("Id  :  Neighbors(weight)\n");
+    for(int i = 0; i<graph->vertices.capacity; i++){
+        elem_Graph *current = graph->vertices.pair[i];
+        if(current == NULL) continue;
+        
+        while(current != NULL){
+            printf("%d  : ", current->key);
+            Edge *current_v = current->value->Edges_list;
+            while(current_v != NULL){
+                printf("%d(%d) ", current_v->destination_id, current_v->weight);
+                current_v = current_v->next_Edge;
+            }
+            printf("\n");
+            current = current->next;
+        }
+        
+    }
+}
+
+void print_graph_values(Graph *graph){
+    if(is_empty_graph(graph)) {
+        printf("Graph is empty\n");
+        return;
+    }
+
+    printf("Value  :  Neighbors(weight)\n");
+    for(int i = 0; i<graph->vertices.capacity; i++){
+        elem_Graph *current = graph->vertices.pair[i];
+        if(current == NULL) continue;
+        
+        while(current != NULL){
+            printf("%d  : ", current->value->value);
+            Edge *current_v = current->value->Edges_list;
+            while(current_v != NULL){
+                int id = current_v->destination_id;
+                Vertex *id_vertex = hash_table_graph_get(&graph->vertices, id);
+                printf("%d(%d) ", id_vertex->value, current_v->weight);
+                current_v = current_v->next_Edge;
+            }
+            printf("\n");
+            current = current->next;
+        }
+    }
+}
+
+void remove_edge_graph(Graph *graph, int from, int to, bool is_id){
+    if(is_id){
+        bool found = false;
+        Vertex *from_v = hash_table_graph_get(&graph->vertices, from);
+        if(from_v == NULL){
+            printf("Id doesn't exist: %d\n", from);
+            return;
+        }
+        Edge *current = from_v->Edges_list;
+        if (current == NULL){
+            printf("Edge doesn't exist\n");
+            return;
+        }
+        if(current->destination_id == to){
+            from_v->Edges_list = current->next_Edge;
+            free(current);
+            if(graph->is_direct) return;
+            found = true;
+            
+        }
+        if(!found){
+            Edge *old = current;
+            current = current->next_Edge;
+            while(current != NULL){
+                if(current->destination_id == to){
+                    old->next_Edge = current->next_Edge;
+                    free(current);
+                    if(graph->is_direct) return;
+                    break;
+                }
+                old = current;
+                current = current->next_Edge;
+            }
+        }
+       
+
+        if(!graph->is_direct){
+            Vertex *to_v = hash_table_graph_get(&graph->vertices, to);
+            if(to_v == NULL){
+            printf("Id doesn't exist: %d\n", to);
+            return;
+        }
+            Edge *current2 = to_v->Edges_list;
+            if(current2 == NULL){
+                printf("Edge doesn't exist\n");
+                return;
+            }
+            if(current2->destination_id == from){
+                to_v->Edges_list = current2->next_Edge;
+                free(current2);
+                return;
+            }
+            Edge *old2 = current2;
+            current2 = current2->next_Edge;
+            while(current2 != NULL){
+                if(current2->destination_id == from){
+                    old2->next_Edge = current2->next_Edge;
+                    free(current2);
+                    return;
+
+                }
+                old2 = current2;
+                current2 = current2->next_Edge;
+            }
+        }
+        printf("Edge doesn't exist\n");
+    }else{
+
+        elem_Graph *current_from = NULL;
+        for(int i = 0; i < graph->vertices.capacity; i++){
+            if(current_from != NULL) break;
+            current_from = graph->vertices.pair[i];
+            while(current_from != NULL){
+                if(current_from->value->value == from) break;
+                current_from = current_from->next;
+            }
+        }
+
+        elem_Graph *current_to = NULL;
+        for(int i = 0; i < graph->vertices.capacity; i++){
+            if(current_to != NULL) break;
+            current_to = graph->vertices.pair[i];
+            while(current_to != NULL){
+                if(current_to->value->value == to) break;
+                current_to = current_to->next;
+            }
+        }
+
+        if(current_from == NULL || current_from->value->value != from) {
+            printf("Value doesn't exist: %d\n", from);
+            return;
+        }
+        if(current_to == NULL || current_to->value->value != to)  {
+            printf("Value doesn't exist: %d\n", to);
+            return;
+        }
+
+        bool found = false;
+        Edge *current = current_from->value->Edges_list;
+        if (current == NULL){
+            printf("Edge doesn't exist\n");
+            return;
+        }
+        if(current->destination_id == current_to->value->id){
+            current_from->value->Edges_list = current->next_Edge;
+            free(current);
+            if(graph->is_direct) return;
+            found = true;
+            
+        }
+        if(!found){
+            Edge *old = current;
+            current = current->next_Edge;
+            while(current != NULL){
+                if(current->destination_id == current_to->value->id){
+                    old->next_Edge = current->next_Edge;
+                    free(current);
+                    if(graph->is_direct) return;
+                    break;
+                }
+                old = current;
+                current = current->next_Edge;
+            }
+        }
+
+        if(!graph->is_direct){
+            
+            Edge *current2 = current_to->value->Edges_list;
+            if(current2 == NULL){
+                printf("Edge doesn't exist\n");
+                return;
+            }
+            if(current2->destination_id == current_from->value->id){
+                current_to->value->Edges_list = current2->next_Edge;
+                free(current2);
+                return;
+            }
+            Edge *old2 = current2;
+            current2 = current2->next_Edge;
+            while(current2 != NULL){
+                if(current2->destination_id == current_from->value->id){
+                    old2->next_Edge = current2->next_Edge;
+                    free(current2);
+                    return;
+
+                }
+                old2 = current2;
+                current2 = current2->next_Edge;
+            }
+        }
+        printf("Edge doesn't exist\n");
+
+
+
+    }
+}
+
+void remove_vertex_graph(Graph *graph, int nb, bool is_id){
+    if(is_id){
+        Vertex *vert = hash_table_graph_get(&graph->vertices, nb);
+        if(vert == NULL){
+            printf("Id doesn't exist: %d\n", nb);
+            return;
+        }
+        Edge *current = vert->Edges_list;
+        while(current != NULL){
+            Edge *next = current->next_Edge;
+            free(current);
+            current = next;
+        }
+        hash_table_graph_delete(&graph->vertices, nb);
+
+        for(int i = 0; i < graph->vertices.capacity; i++){
+            elem_Graph *current = graph->vertices.pair[i];
+            if(current == NULL) continue;
+        
+            while(current != NULL){
+                
+                Edge *current_v = current->value->Edges_list;
+
+                while(current_v != NULL && current_v->destination_id == nb){
+                    current->value->Edges_list = current_v->next_Edge;
+                    Edge *s = current_v->next_Edge;
+                    free(current_v);
+                    current_v = s;
+
+                }
+
+                if(current_v != NULL){
+                    Edge *old = current_v;
+                    current_v = current_v->next_Edge;
+                    while(current_v != NULL){
+                        if(current_v->destination_id == nb){
+                            old->next_Edge = current_v->next_Edge;
+                            free(current_v);
+                            current_v = old->next_Edge;
+                        }else{
+                            old = current_v;
+                            current_v = current_v->next_Edge;
+
+                        }
+                    
+                    }
+
+                }
+                
+                current = current->next;
+            }
+        
+        }
+        
+
+    }else{
+        elem_Graph *current_from = NULL;
+        for(int i = 0; i < graph->vertices.capacity; i++){
+            if(current_from != NULL) break;
+            current_from = graph->vertices.pair[i];
+            while(current_from != NULL){
+                if(current_from->value->value == nb) break;
+                current_from = current_from->next;
+            }
+        }
+        if(current_from == NULL || current_from->value->value != nb){
+            printf("Value doesn't exist: %d\n", nb);
+            return;
+        }
+
+        Vertex *vert = current_from->value;
+        int id = vert->id;
+        
+        Edge *current = vert->Edges_list;
+        while(current != NULL){
+            Edge *next = current->next_Edge;
+            free(current);
+            current = next;
+        }
+        hash_table_graph_delete(&graph->vertices, id);
+
+        for(int i = 0; i < graph->vertices.capacity; i++){
+            elem_Graph *current = graph->vertices.pair[i];
+            if(current == NULL) continue;
+        
+            while(current != NULL){
+                
+                Edge *current_v = current->value->Edges_list;
+
+                while(current_v != NULL && current_v->destination_id == id){
+                    current->value->Edges_list = current_v->next_Edge;
+                    Edge *s = current_v->next_Edge;
+                    free(current_v);
+                    current_v = s;
+
+                }
+
+                if(current_v != NULL){
+                    Edge *old = current_v;
+                    current_v = current_v->next_Edge;
+                    while(current_v != NULL){
+                        if(current_v->destination_id == id){
+                            old->next_Edge = current_v->next_Edge;
+                            free(current_v);
+                            current_v = old->next_Edge;
+                        }else{
+                            old = current_v;
+                            current_v = current_v->next_Edge;
+
+                        }
+                    
+                    }
+
+                }
+                
+                current = current->next;
+            }
+        
+        }
+
+    }
+
+}
