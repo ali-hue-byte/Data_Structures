ba0bbd8e4478c2fe9ea96bcc74a36e4f2d5499e3
ali-hue-byte <elguelouiali@gmail.com>
2026-06-25 14:30:17 +0200

    Update Graph.h

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -1,6 +1,7 @@
 #ifndef Graph_H
 #define Graph_H
 
+#include "../Array/dynamic_array.h"
 
 typedef struct Vertex Vertex;
 
@@ -33,6 +34,7 @@ void hash_table_graph_update(Hash_Table_Graph *h_t, int key, Vertex *value);
 
 
 
+
 typedef struct Edge { 
     int weight;
     int destination_id;
@@ -44,6 +46,7 @@ struct Vertex {
     int id;
     int value;
     Edge *Edges_list;
+    
 };
 
 typedef struct {
@@ -53,7 +56,21 @@ typedef struct {
     bool is_direct;
 } Graph;
 
-
+Graph create_graph(bool is_weighted, bool is_direct);
+int add_vertex_graph(Graph *graph, int value);
+void add_edge_graph(Graph *graph, int from, int to, int weight, bool is_id);
+int get_edge_weight_graph(Graph *graph, int from, int to, bool is_id);
+bool edge_exists_graph(Graph *graph, int from, int to, bool is_id);
+Dynamic_Array get_neighbors_ids(Graph *graph, int id);
+Dynamic_Array get_neighbors_values(Graph *graph, int value);
+bool is_empty_graph(Graph *graph);
+int graph_length(Graph *graph);
+bool vertex_id_exists_graph(Graph *graph, int id);
+bool vertex_value_exists_graph(Graph *graph, int value);
+void print_graph_ids(Graph *graph);
+void print_graph_values(Graph *graph);
+void remove_edge_graph(Graph *graph, int from, int to, bool is_id);
+void remove_vertex_graph(Graph *graph, int nb, bool is_id);
 
 
 #endif
